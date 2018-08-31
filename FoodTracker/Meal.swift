658e172f3503d2bec88b03741515d7ea9571583a
Gabriel Palmer <gabepalmer@gabriels-macbook-air.local>
2018-08-31 08:57:06 -0600

    Updated until the addition of the Meal file

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,15 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Gabriel Blaine Palmer on 8/31/18.
+//  Copyright © 2018 Gabriel Blaine Palmer. All rights reserved.
+//
+
+import UIKit
+
+class Meal: UIStackView {
+
+    
+
+}
